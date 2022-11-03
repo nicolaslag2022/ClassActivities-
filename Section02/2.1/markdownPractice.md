f8fab19dcd6752810153c7a4fbbd28bab26d5fc2
nicolaslag2022 <nick.goforth@gmail.com>
2022-11-02 21:40:11 -0500

    up to activity 2.2

---
+++ Section02/2.1/markdownPractice.md
@@ -0,0 +1,15 @@
+# Favorite Sports Teams
+## Dallas Cowboys
+
+- Dak Prescott
+- Tony Pollard
+- Tony Romo
+
+## Buffalo Bills
+
+1. Josh Allen
+
+2. Stefon Diggs
+
+3. Devin Singletary
+
---
+++ Section02/2.2/2.2.1-JSVariablesAndPrimitives.js
@@ -0,0 +1,27 @@
+// create a constant variable (const)
+const myName = "Nick Goforth"
+
+// create a variable that can be reassigned (let)
+let age = 29
+
+// create three variables and assign them values of different data types (=)
+let favoritecolor = "blue";
+let hasDog = true;
+let numberofLegs = 2;
+let language = "JavaScript"
+
+// print the types of two variables that reference two different data types (typeof)
+// TIP: console.log() prints a value in the console/terminal
+console.log(myName) ;
+console.log(numberOfLegs) ;
+
+// change the value referenced by a variable (dynamic typing)
+numberOfLegs = 4;
+
+// print the type of the variable you just changed (typeof)
+console.log(numberOfLegs);
+// create variables and give them values to complete the sentences that will print  below.
+// Which variables need to be created?
+// What type of data needs to go in each variable?
+
+console.log("Hello, my name is " + myName + ", and I am learning " + JavaScript + ". I have been practicing for only " +  age + " weeks, so my status as a master is: " + true + ".");
