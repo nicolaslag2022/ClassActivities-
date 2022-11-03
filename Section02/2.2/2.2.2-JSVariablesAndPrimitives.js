8d5e8751d8c2eeda7d63f310c30072747dbeb510
nicolaslag2022 <nick.goforth@gmail.com>
2022-11-03 11:32:29 -0500

    Updated 2.2.2 class activity

--- Section02/2.2/2.2.2-JSVariablesAndPrimitives.js
+++ Section02/2.2/2.2.2-JSVariablesAndPrimitives.js
@@ -2,10 +2,10 @@
 let name = "Nick";
 
 // create a variable called "favoriteSong" that references a string
-let favoriteSong = "Wondering Around";
+let favoriteSong = "Hey Jude";
 
 // create a variable called "wage" that references a number
-let wage = 20.50
+let wage = 20.50;
 
 // create a variable called "age" that references a number
 let age = 35;
@@ -14,25 +14,33 @@ let age = 35;
 let onlyChild = false;
 
 // create a variable called "satisfied" that references a boolean
-let satisfied = true;
+let satisfied = false;
 
 // create a variable called "favoriteThing" that references ANY primitive value
 // print the data type of "favoriteThing"
-
+let favoriteThing = "Pizza";
+console.log(favoriteThing);
 
 // You heard a new song that became your favorite.
 // Reassign the variable "favoriteSong"
+favoriteSong = "Yellow Submarine";
 
 // You no longer like the type of data stored in "favoriteThing"
 // Reassign "favoriteThing" to a different primitive data type and print its type
-
+let favoriteThing = 35;
+console.log(35);
 // Happy Birthday!
 // Reassign "age" to a new value
+age = 36;
 
 // You got a big raise at work.
 // Reassign "wage" to a new value
+wage = 24.75;
 
 // Your raise changed your job satisfaction.
 // Assign "satisfied" to true
+satisfied = true;
 
 // use "console.log()" and "+" to print a sentence that includes two of the above variables
+console.log("Hello my name is " + myName + " I am " + age + "years old!")
+
